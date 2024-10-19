81a1f36e217a4b6ea858780c114ea56ca877fa3d
Ejjay <ejalloso021@gmail.com>
2024-10-19 22:34:35 +0800

    Update SigninForm.tsx

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -56,11 +56,11 @@ const SigninForm = () => {
       <div className="sm:w-420 flex-center flex-col">
   <img src="/assets/images/pcmi.png" alt="logo" style={{ width: "80px" }} />
 
-        <h1 className="h3-bold md:h2-bold pt-5 sm:pt-12">
-         
-          Welcome to PCMI!
-          
-        </h1>
+        
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12 tracking-wider sm:tracking-widest">
+  Welcome to PCMI!
+</h2>
+        
         <p className="text-light-3 small-medium md:base-regular mt-2">
          You can Login here.
         </p>
