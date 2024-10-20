5134b65940dfb9ff924f16a8be8a330a9866a57c
Ejjay <ejalloso021@gmail.com>
2024-10-20 12:27:09 +0800

    Update FileUploader.tsx

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -52,12 +52,12 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
           />
 
           <h3 className="base-medium text-light-2 mb-2 mt-6">
-            Drag photo here
+            Add photo here
           </h3>
-          <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG</p>
+          <p className="text-light-4 small-regular mb-6">PNG, JPG, SVG</p>
 
           <Button type="button" className="shad-button_dark_4">
-            Select from computer
+            Select from Device
           </Button>
         </div>
       )}
